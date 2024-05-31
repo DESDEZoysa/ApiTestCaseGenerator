d80a0a95eaa477a620d9b7931a45d769cfbbb339
Eranga Sampath <ErangaS@embla.asia>
2024-05-31 11:23:54 +0530

    rearange test collection

--- app.py
+++ app.py
@@ -3,16 +3,20 @@ import json
 import model.constant.filePathConstant as FilePathConstant
 from service.apiDefinitionReader import ApiDefinitionReader
 from service.postmanCollectionGenerator import PostmanCollectionGenerator
+from service.postmanCollectionFormatter import PostmanCollectionFormatter
 
 apiDefinitionReader = ApiDefinitionReader()
 postmanCollectionGenerator = PostmanCollectionGenerator()
+postmanCollectionFormatter = PostmanCollectionFormatter()
 
 with open(FilePathConstant.API_DEFINITION_PATH) as file:
     data = json.load(file)    
 requestDic = apiDefinitionReader.getUrlDic(data)
-collection = postmanCollectionGenerator.generatePostmanCollection(requestDic,data)
+postmanCollection = postmanCollectionGenerator.generatePostmanCollection(requestDic,data)
+postmanCollection = postmanCollectionFormatter.formatPostmanCollection(postmanCollection)
 with open(FilePathConstant.POSTMAN_COLLECTION_PATH, "w") as file:    
-    json.dump(collection, file, indent=4)
+    json.dump(postmanCollection, file, indent=4)
+
 
 
 
